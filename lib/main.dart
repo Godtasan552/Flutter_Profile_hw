4a505ba0ed7340ede7ebac891e051d8d436fa61e
Godtasan552 <werawat7903@gmail.com>
2025-06-27 14:51:39 +0700

    add image layout and text

--- lib/main.dart
+++ lib/main.dart
@@ -12,15 +12,106 @@ class MainApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Hello World!', style: TextStyle(color: Colors.white)),
+          title: Text('My Profile', style: TextStyle(color: Colors.white)),
           backgroundColor: Colors.blue,
         ),
         body: Column(
           mainAxisAlignment: MainAxisAlignment.start,
           children: [
-            Image.asset('assets/image/Human.jpg'),
+            Image.asset('assets/image/Human.jpg', width: 330, height: 400, fit: BoxFit.cover),
+            const SizedBox(height: 10),
+            Text("Student Name: Weerawat ", 
+            style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold
+            )),
+            const SizedBox(height: 20),
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: Text(
+                "นักศึกษาสาขาวิทยาการคอมพิวเตอร์\nคณะศิลปศาสตร์และวิทยาศาสตร์\nมหาวิทยาลัยราชภัฏศรีสะเกษ",
+                style: TextStyle(fontSize: 20, ),
+                textAlign: TextAlign.center,
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 5),
+              child: Container(
+                height: 1,
+                color: Colors.grey,
+              ),
+            ),
+            SizedBox(height: 5),
+            Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Row(
+                    children: [
+                      Icon(Icons.star,
+                      color: Colors.pink,),
+                      Icon(Icons.star,
+                      color: Colors.pink,),
+                      Icon(Icons.star,
+                      color: Colors.pink,),
+                      Icon(Icons.star,
+                      color: Colors.pink,),
+                      Icon(Icons.star,
+                      color: Colors.pink,),
+                    ],
+                  ),
+                  
+                  Text(
+                    "500 Reviews",
+                    style: TextStyle(fontSize: 20,),
+                    textAlign: TextAlign.center,
+              
+                  )
+                ],
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 5),
+              child: Container(
+                height: 1,
+                color: Colors.grey,
+              ),
+            ),
+            const SizedBox(height: 10),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Column(
+                  children: [
+                    Icon(Icons.phone, color: Colors.blue, size: 30),
+                    Text(
+                      "Phone",
+                      style: TextStyle(fontSize: 20),
+                    ),
+                  ],
+                ),
+                Column(
+                  children: [
+                    Icon(Icons.email, color: Colors.blue, size: 30),
+                    Text(
+                      "Email",
+                      style: TextStyle(fontSize: 20),
+                    ),
+                  ],
+                ),
+                Column(
+                  children: [
+                    Icon(Icons.facebook_outlined, color: Colors.blue, size: 30),
+                    Text(
+                      "Social",
+                      style: TextStyle(fontSize: 20),
+                    ),
+                  ],
+                ),
+              ],
+            )
           ],
         ),
+        
       ),
     );
   }
